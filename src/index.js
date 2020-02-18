defc1eb90b8bfebfbf32bc00cef08eb74df744e2
ajaykrishnalla <ajaykrish.nalla@gmail.com>
2020-02-18 12:16:22 +0530

    tricky javascript

--- src/index.js
+++ src/index.js
@@ -1,21 +1,57 @@
-const add = x => {
-  return y => {
-    return z => {
-      console.dir(x);
-      return x + y + z;
-    };
-  };
-};
+// const add = x => {
+//   return y => {
+//     return z => {
+//       return x + y + z;
+//     };
+//   };
+// };
 
-console.log(add(1)(3)(3));
-for (let i = 0; i <= 5; i++) {
-  setTimeout(() => {
-    console.log(i);
-  }, i * 1000);
-} ////0,1,2,3,4,5
+// // console.log(add(1)(3)(3));
+// for (let i = 0; i <= 5; i++) {
+//   setTimeout(() => {
+//     // console.log(i);
+//   }, i * 1000);
+// } ////0,1,2,3,4,5
 
-for (var i = 0; i < 5; i++) {
-  setTimeout(function() {
-    console.log(i);
-  }, i * 1000);
-} /// 5555555555555
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(function() {
+//     // console.log(i);
+//   }, i * 1000);
+// } /// 5555555555555
+
+// function foo1()
+// {
+//   return {
+//       bar: "hello"
+//   };
+// }
+
+// function foo2()
+// {
+//   return
+//   {
+//       bar: "hello"
+//   };
+// }
+
+// console.log(foo1());
+// console.log(foo2())
+
+// for (let i = 0; i < 5; i++) {
+//   var btn = document.createElement('button');
+//   btn.appendChild(document.createTextNode('Button ' + i));
+//   btn.addEventListener('click', function(){ console.log(i); });
+//   document.body.appendChild(btn);
+// }
+
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl ();
+
+var arr = [1, 2, 3];
+
+arr[10] = 99;
+console.log(arr);
