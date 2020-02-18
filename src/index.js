5024ae696bf653d625fb3600622acc62595d910f
ajaykrishnalla <ajaykrish.nalla@gmail.com>
2020-02-18 11:49:14 +0530

    diff between let and var

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const add = x => {
   return y => {
     return z => {
+      console.dir(x);
       return x + y + z;
     };
   };
 };
 
 console.log(add(1)(3)(3));
+for (let i = 0; i <= 5; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, i * 1000);
+} ////0,1,2,3,4,5
+
+for (var i = 0; i < 5; i++) {
+  setTimeout(function() {
+    console.log(i);
+  }, i * 1000);
+} /// 5555555555555
